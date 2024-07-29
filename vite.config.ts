697ab1b6b67495115fbf4dba1b86fc909f4b7eb6
Milin7 <isaac_martinez.g@hotmail.com>
2024-07-29 13:05:36 -0600

    final workflow commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Milin7/tip-calculator-with-ts/",
+  base: "/tip-calculator-with-ts",
 })
